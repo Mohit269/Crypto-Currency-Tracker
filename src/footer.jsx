3e80423c4162d54a3e397342a2fe08f56df71f77
unknown <Mohit269>
2021-09-15 11:54:56 +0530

    first commit

---
+++ src/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer(){
+    return(
+    <div className="footer">
+        <p>Made with by &copy; M.Developers</p>
+    </div>
+    );
+}
+export default Footer;
